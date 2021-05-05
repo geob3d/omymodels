b03927a07939e9adf2c6adab3d433f9051d88261
geob3d <30846371+geob3d@users.noreply.github.com>
2021-05-05 01:11:02 -0400

    Created the core.py file for gin-gorm

--- omymodels/__init__.py
+++ omymodels/__init__.py
@@ -1,4 +1,8 @@
-from omymodels.common import create_models
+# prod
+# from omymodels.common import create_models
+
+# __all__ = ["create_models"]
 
 
-__all__ = ["create_models"]
+# dev
+from omymodels.common import *
--- omymodels/gin-gorm/core.py
+++ omymodels/gin-gorm/core.py
@@ -1,11 +1,12 @@
-#### Delete for development
-from omymodels import common
+#### Dev
+from omymodels import *
 from icecream import ic
+
+###Prod
 from typing import Optional, List, Dict
-
-
-from simple_ddl_parser import DDLParser, parse_from_file
-
+from omymodels.pydantic import templates as pt
+from omymodels.utils import create_class_name, type_not_found, enum_number_name_list
+from omymodels.pydantic.types import types_mapping, datetime_types
 
 ddl = """
 CREATE table user_history (
@@ -17,51 +18,69 @@ CREATE table user_history (
     ,event_time            timestamp not null default now()
     ,comment           varchar(1000) not null default 'none'
     ) ;
-
-
 """
-
-
-def get_tables_information(
-    ddl: Optional[str] = None, ddl_file: Optional[str] = None
-) -> List[Dict]:
-    if not ddl_file and not ddl:
-        raise ValueError(
-            "You need to provide one of above argument: ddl with string that "
-            "contains ddl or ddl_file that contains path to ddl file to parse"
-        )
-    if ddl:
-        tables = DDLParser(ddl).run(group_by_type=True)
-    elif ddl_file:
-        tables = parse_from_file(ddl_file, group_by_type=True)
-    return tables
-
-
-def remove_quotes_from_strings(item: Dict) -> Dict:
-    for key, value in item.items():
-        if key.lower() != "default":
-            if isinstance(value, list):
-                value = iterate_over_the_list(value)
-                item[key] = value
-            elif isinstance(value, str) and key != "default":
-                item[key] = value.replace('"', "")
-            elif isinstance(value, dict):
-                value = remove_quotes_from_strings(value)
-    return item
-
-
-def iterate_over_the_list(items: List) -> str:
-    """ simple ddl parser return " in strings if in DDL them was used, we need to remove them"""
-    for item in items:
-        if isinstance(item, dict):
-            remove_quotes_from_strings(item)
-        elif isinstance(item, str):
-            new_item = item.replace('"', "")
-            items.remove(item)
-            items.append(new_item)
-    return items
-
-
 data = get_tables_information(ddl=ddl)
 data = remove_quotes_from_strings(data)
 ic(data)
+
+
+## Prod
+
+
+class ModelGenerator:
+    def __init__(self):
+
+        self.imports = set([pt.base_model])
+        self.types_for_import = ["Json"]
+        self.datetime_import = False
+        self.typing_imports = set()
+        self.enum_imports = set()
+        self.custom_types = {}
+        self.uuid_import = False
+
+    def generate_attr(self, column: Dict, defaults_off: bool) -> str:
+        if column["nullable"]:
+            self.typing_imports.add("Optional")
+            column_str = pt.pydantic_optional_attr
+        else:
+            column_str = pt.pydantic_attr
+
+        if "." in column["type"]:
+            _type = column["type"].split(".")[1]
+        else:
+            _type = column["type"].lower().split("[")[0]
+        if self.custom_types:
+            column_type = self.custom_types.get(_type, _type)
+            if isinstance(column_type, tuple):
+                _type = column_type[1]
+                column_type = column_type[0]
+            if column_type != type_not_found:
+                column_type = f"{column_type}({_type})"
+        if _type == _type:
+            _type = types_mapping.get(_type, _type)
+        if _type in self.types_for_import:
+            self.imports.add(_type)
+        elif "datetime" in _type:
+            self.datetime_import = True
+        elif "[" in column["type"]:
+            self.typing_imports.add("List")
+            _type = f"List[{_type}]"
+        if _type == "UUID":
+            self.uuid_import = True
+
+        column_str = column_str.format(arg_name=column["name"], type=_type)
+        if column["default"] and defaults_off is False:
+            if column["type"].upper() in datetime_types:
+                if "now" in column["default"]:
+                    # todo: need to add other popular PostgreSQL & MySQL functions
+                    column["default"] = "datetime.datetime.now()"
+                elif "'" not in column["default"]:
+                    column["default"] = f"'{column['default']}'"
+            column_str += pt.pydantic_default_attr.format(default=column["default"])
+
+        return column_str
+
+
+##Dev
+model_gen = ModelGenerator()
+model_gen.generate_attr(data, 0)
